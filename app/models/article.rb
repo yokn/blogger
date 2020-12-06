b78ff4bfef05f4cc4be01bfd39a30d89146713a9
yokn <yokn@protonmail.com>
2020-12-06 20:06:53 +0300

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,9 @@ class Article < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
+
   def tag_list
     tags.join(', ')
   end
