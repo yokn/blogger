21255a605a3fe366f6bb9fc55801f13f855048ae
yokn <yokn@protonmail.com>
2020-12-07 12:03:34 +0300

    switch to postgres

--- db/migrate/20201206071356_create_comments.rb
+++ db/migrate/20201206071356_create_comments.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :author_name
+      t.text :author_name
       t.text :body
       t.references :article, null: false, foreign_key: true
 
--- db/migrate/20201206091617_create_tags.rb
+++ db/migrate/20201206091617_create_tags.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateTags < ActiveRecord::Migration[6.0]
   def change
     create_table :tags do |t|
-      t.string :name
+      t.text :name
 
       t.timestamps
     end
